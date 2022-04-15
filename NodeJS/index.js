47f1e8b3d9747a62763a5ca1cb53db997f26b06b
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-04-15 19:21:13 +0200

    Update index.js
    file type

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,6 +1,8 @@
 // Packages
 const fs = require("fs");
-
+let file = "tak.png.jpg".split(".");
+let file_type = file[file.length - 1];
+console.log(file_name, file_type);
 let old_file = fs.readFileSync("tak.jpg", { encoding: "base64" });
 
 let new_file = fs.writeFileSync("[base64]tak_jpg", old_file);
